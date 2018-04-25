fe62f24dca151b9587889fad192a079ec6eb98b8
Aaron Wang <aaronw94@gmail.com>
2018-04-25 01:53:57 -0500

    pruned attributes

--- graph_anal.py
+++ graph_anal.py
@@ -10,10 +10,25 @@ def all_pairs_sp(G):
         file.write(json.dumps(paths))
 
 def main():
-    G = nx.read_gpickle('SJ.gpickle')
-    ox.plot_graph(G)
+    G = nx.read_gpickle('./SJ.gpickle')
+    #ox.plot_graph(G)
 
-    all_pairs_sp(G)
+    T = G.copy()
+
+    for (n,d) in T.nodes(data = True):
+        d.clear()
+
+    print(T.nodes(data = True))
+
+    input('Press enter to continue')
+
+    for (n1,n2,d) in T.edges(data = True):
+        len_val = d['length']
+        d.clear()
+        d['length'] = len_val
+
+    print(T.edges(data = True))
+    all_pairs_sp(T)
 
 if __name__ == "__main__":
     main()
